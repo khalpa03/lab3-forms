19621291fbcd4fc9512c6336ebf3f210aa0f1276
Алексей Халманов <khalmanov2003@mail.ru>
2021-10-08 14:37:44 +0800

    Сделал сохранение в полях после закытия, и ошибку на ввод.

--- lab3 forms/Form1.cs
+++ lab3 forms/Form1.cs
@@ -12,20 +12,41 @@ namespace lab3_forms
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
-            
+
             InitializeComponent();
+            word1.Text = Properties.Settings.Default.word1.ToString();
+            word2.Text = Properties.Settings.Default.word2.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-                string word1 = this.word1.Text;
-                string word2 = this.word2.Text;
-            
+            string word1;
+            string word2;
+            try
+            {
+                word1 = this.word1.Text;
+                word2 = this.word2.Text;
+                Properties.Settings.Default.word1 = word1;
+                Properties.Settings.Default.word2 = word2;
+                Properties.Settings.Default.Save();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (Logic.Compare(word1, word2) == "")
+            {
+                MessageBox.Show("Некорректный ввод", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
                 MessageBox.Show(Logic.Compare(word1, word2), "Результат", MessageBoxButtons.OK);
-            
+            }
         }
-       
+
     }
 }
